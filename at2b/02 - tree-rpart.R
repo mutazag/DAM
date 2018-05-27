ff9f1d0d7e1fff759aa524cbee8822e6783aa543
mutaz mdsi (on book) <mutaz.m.abughazaleh@student.uts.edu.au>
2018-05-27 22:26:07 +1000

    DAM at2b added pruning to decision tree

--- at2b/02 - tree-rpart.R
+++ at2b/02 - tree-rpart.R
@@ -9,6 +9,7 @@ library(caret)
 library (rpart)
 library(rpart.plot)
 library(mlbench)
+library(randomForest)
 
 repurchase_levels <- c(0,1)
 repurchase_labels <- c("no", "yes")
@@ -28,7 +29,7 @@ dff <- read_csv("./dff.csv",
                ))
 
 
-#### functions #### 
+#### functions  #### 
 
 train_test_rpart <- function(formula = repurchase ~ ., 
                        train = dff_train, 
@@ -100,6 +101,8 @@ train_test_rpart <- function(formula = repurchase ~ .,
   
   
 }
+
+
 
 #### split ####
 
@@ -201,3 +204,135 @@ test3$perf
 test3$train_confusionMatrix
 test3$test_confusionMatrix
 
+
+
+#### compare all example models ####
+
+report_model <- function(model_result,desc =""){
+  obj_name <- deparse(substitute(model_result))
+  return(
+    data.frame(
+      name = obj_name,
+      model.type = model_result$model.type, 
+      desc = desc, 
+      model_result$perf
+    )
+  )
+}
+
+
+model_results <- rbind(
+report_model(test1, "all variables as predictors"),
+report_model(test2, "age and gender but not other categorical variables"),
+report_model(test3, "no categorical variables")
+)
+
+
+
+#### change seed and run best model (test1) ####
+
+# we will do this to test the stabilit of the tree and determine if pruning is needd
+
+seed_split_train_test <- function(seed = 42){
+  
+  set.seed(seed)
+  # split 70/30 split
+  train <- createDataPartition(y = dff$repurchase, p = .7, list = F)
+  dff_train <- dff[train, ]
+  dff_test <- dff[-train, ]
+  
+  test1 <- train_test_rpart()
+  return(test1)
+}
+
+
+seedtest_42 <- seed_split_train_test(42)
+seedtest_14 <- seed_split_train_test(14)
+seedtest_22 <- seed_split_train_test(22)
+
+seedtest_results <- rbind(
+  report_model(seedtest_42, "seedtest_42"),
+  report_model(seedtest_14, "seedtest_14"),
+  report_model(seedtest_22, "seedtest_22")
+)
+
+
+prp(seedtest_42$fitted.model)
+prp(seedtest_14$fitted.model)
+prp(seedtest_22$fitted.model)
+
+
+#### prunning of test 1 model to avoid overfitting the tree ####
+
+prune_model <- function(model_result){
+  model <- model_result$fitted.model
+  # get minimum cost complexity pruning
+  #plotcp(model)
+  opt <- which.min(model$cptable[,"xerror"])
+  # take it one step up 
+  cp <- model$cptable[(opt-2), "CP"]
+  
+  # prune model with select minimum cp - 1 
+  pruned_model <- prune(model, cp )
+  #prp(pruned_model)
+  train <- model_result$train %>% select(-pred_class)
+  train$pred_class <- predict(pruned_model,
+                                    train %>% select(-repurchase),
+                                    type="class")
+  
+  test <- model_result$test %>% select(-pred_class,
+                                       -pred_propability,
+                                       -pred_class2)
+  test$pred_class <- predict(pruned_model, 
+                                  test %>% select(-repurchase),
+                                  type="class")
+  test$pred_propability <- predict(pruned_model,
+                                         test %>% select(-repurchase),
+                                         type="prob")
+  test$pred_class2 <- ifelse(as.vector(test$pred_propability[,"TRUE"]) >=  .5, TRUE, FALSE)
+  
+  # print("Confusion Matrix on TRain Data Set")
+  train_confusionMatrix <- confusionMatrix(
+    data = as.factor(train$pred_class), 
+    as.factor(train$repurchase))
+  
+  # print("Confusion Matrix on Test Data Set")
+  test_confusionMatrix <- confusionMatrix(
+    data = as.factor(test$pred_class), 
+    as.factor(test$repurchase))
+  
+  
+  
+  # store accuracy, precision, recall and F1 from test partition 
+  Accuracy <- test_confusionMatrix$overall[["Accuracy"]]
+  Sensitivity <- test_confusionMatrix$byClass[["Sensitivity"]]
+  Precision <- test_confusionMatrix$byClass[["Precision"]]
+  Recall <- test_confusionMatrix$byClass[["Specificity"]]
+  F1 <- 2 * ((Precision*Recall)/(Precision+Recall))
+  
+  
+  return(list(
+    model.type = "rpart.pruned", 
+    fitted.model = pruned_model, 
+    train = train, 
+    train_confusionMatrix = train_confusionMatrix, 
+    test = test, 
+    test_confusionMatrix = test_confusionMatrix, 
+    perf = data.frame(Accuracy, 
+                      Sensitivity, 
+                      Precision, 
+                      Recall, 
+                      F1)
+  ))
+}
+
+
+
+test3_pruned <- prune_model(test3)
+prp(test3_pruned$fitted.model)
+prp(test3$fitted.model)
+
+prunning_report <- rbind(
+  report_model(test3, "test3 before prunning"), 
+  report_model(test3_pruned, "test3 after prunning")
+)
